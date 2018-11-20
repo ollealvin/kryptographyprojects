f5e092278b5214158da327a5213c09fac06031e0
ollealvin <ollealvin@users.noreply.github.com>
2018-11-20 19:55:52 +0100

    Optimized the factorbase numbers computation and added a pause

--- project1.py
+++ project1.py
@@ -11,83 +11,73 @@ from numpy.linalg import svd as svd
 import math
 import os
 import subprocess
+import time
 
 def main(argv):
-    haveSolutions = False;
+    haveSolutions =True;
     #N = argv[0]
     print(argv)
 
-    N = 145968946107052219367611
-    L = 500
+    #N = 145968946107052219367611
+    #N = 10967535067
+   # N = 16637
+    N = 392742364277 #this number does not work.
+    L = 1000
     F = make_prime_list(L)
     r_list, M = r_generator(F, N, L)
     f = open("input.txt", "w")
     f.write("%d %d" % (L, len(F)))
     
-    if not haveSolutions:
-        for i in range(L):
-            f.write("\n")
-            for j in range(len(F)):
-                f.write("%d" % M[i,j])
-                if j != len(F):
-                    f.write(" ")
+    for i in range(L):
+        f.write("\n")
+        for j in range(len(F)):
+            f.write("%d" % M[i,j])
+            if j != len(F):
+                f.write(" ")
                 
-        f.close
-    else:
-        with open("output.txt") as file:
+    f.close()
+    print("pause");
+    programPause = input("Run gaussbin and ress the <ENTER> key to continue...")
+    print("unpause")
+    
+    with open("output.txt") as file:
+        
+        counter = 1
+        for line in file.readlines():
+            factor_dict = {}
+            if counter == 1:
+                counter += 1
+                continue
+            row_s = line.strip().split(" ")
+            r_product = 1
+            factor_product = 1
+            factor_dict= {}
+            for i in range(len(row_s)): 
+                if row_s[i] == '1':
+                    r_product *= r_list[i][0]
+                    r_product = r_product % N
+                    for j in r_list[i][1]:
+                        factor_product *= j
+                        if j not in factor_dict:
+                            factor_dict[j] = 1
+                        else:
+                            factor_dict[j] = factor_dict[j] + 1
             
-            counter = 1
-            for line in file.readlines():
-                factor_dict = {}
-                if counter == 1:
-                    counter += 1
-                    continue
-                row_s = line.strip().split(" ")
-                r_product = 1
-                factor_product = 1
-                factor_dict= {}
-                for i in range(len(row_s)): 
-                    if row_s[i] == '1':
-                        r_product *= r_list[i][0]
-                        r_product = r_product % N
-                        for j in r_list[i][1]:
-                            factor_product *= j
-                            if j not in factor_dict:
-                                factor_dict[j] = 1
-                            else:
-                                factor_dict[j] = factor_dict[j] + 1
-                
-                for k in factor_dict:
-                    if factor_dict[k] % 2 != 0:
-                        print("ERROR!!!!")
-                    factor_product = factor_product * k**(factor_dict[k]/2)
-                    factor_product = factor_product % N
-                v = factor_product - r_product
-                v = math.floor(v)
-                v = v%N
-                if math.gcd(v, N) != 1:
-                    p = math.gcd(v,N)
-                    q = math.floor(N/p)
-                    print(p,q)
-                    break
-                        
-                        
-#    f_out = open("output.txt", "w")
-#    f_out.close
-    #if os.path.isfile("C:/Users/Olle/Documents/GitHub/kryptographyprojects/GaussBin.exe"):
-     #   print("is a file!")
-        
-    #args = "C:/Users/Olle/Documents/GitHub/kryptographyprojects/GaussBin.exe input.txt output.txt"
-    #os.system("C:/Users/Olle/Documents/GitHub/kryptographyprojects/GaussBin.exe input.txt output.txt")
-    #subprocess.run(args)
-    
-    
-#    FNULL = open(os.devnull, 'w')    #use this if you want to suppress output to stdout from the subprocess
-#    args = "GaussBin.exe " + "input.txt " + "input.txt"
-#    subprocess.call(args, stdout=FNULL, stderr=FNULL, shell=False)
-#    
-#    #os.system("GaussBin.exe input.txt output.txt")
-        
+            for k in factor_dict:
+                if factor_dict[k] % 2 != 0:
+                    print("ERROR!!!!")
+                factor_product = factor_product * k**(factor_dict[k]>>1)
+                factor_product = factor_product % N
+            v = factor_product - r_product
+            v = math.floor(v)
+            v = v%N
+            if math.gcd(v, N) != 1 and math.gcd(v, N) != N:
+                p = math.gcd(v,N)
+                q = math.floor(N/p)
+                print(p,q)
+                return
+                break
+        print("No solutions")
     
 
 
@@ -112,21 +102,27 @@ def r_generator(F, N, L):
     Returns a list of  L tuples, the first item in the tuple being r and the second7
     being the factors of r^2 mod N.
     """
+    start = time.clock()
     tuples = []
-    
-    
+    sum = 2;
+    MAX_SUM = 10000000;
     M = np.zeros((L,len(F)),dtype='int')
     created_rows = 0
-    for j in range(1, N):
-        for k in range(1, L + math.floor(L/2)):
+    numbers_tested = 0;
+    while sum < MAX_SUM:
+        for k in range(1,sum-1):
+            j = sum-k
             r = (math.floor(math.sqrt(k*N)) + j)
             r_squared = r**2 % N
             factors = []
+            numbers_tested += 1
 
             for p in F:
                 while r_squared % p == 0 and r_squared !=0:
                     r_squared = r_squared / p
                     factors.append(p)
+                if r_squared == 1:
+                    break
 
             if r_squared == 1:
                 row = np.zeros(len(F), dtype='int')
@@ -144,14 +140,19 @@ def r_generator(F, N, L):
                     M[created_rows, :] = row
                     tuples.append((r, factors))
                     created_rows += 1
-                    print("Added row", created_rows)
+                    print("Added row", created_rows, r,k,j)
                 if created_rows == L:
+                    stop = time.clock()
+                    print("Finished computing numbers factored by our factor base, tested %d numbers, time: %f" % (numbers_tested, stop-start) )
                     return tuples, M
-
+            
+        sum += 1
+        
+    if sum == MAX_SUM:
+        print("Tried %d numbers but did not find enough!" %(MAX_SUM))
     if len(tuples) != L:
         print("WARNING: The r_generator needs improvement!!!", len(tuples))
         
-
 
 if __name__ == "__main__": main(sys.argv[1:])
 
